b9faa62590476c6021c4e0e7460a15bd9734fd1b
Ruan Petterson <ruan.petterson@unesp.br>
2020-06-20 23:09:23 -0300

    DatePipe example config

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [
+    DatePipe
+  ]
+})
+
+export class HomeModule { }
